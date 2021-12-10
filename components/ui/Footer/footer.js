5878c77b7498635ae3c2d8837999cc556d8ecda5
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-12-10 22:33:38 +0900

    ディレクトリ構造変更

---
+++ components/ui/Footer/footer.js
@@ -0,0 +1,10 @@
+export function Footer() {
+  return(
+    <div className="bg-slate-100 h-[88px]">
+      <div className="flex justify-center items-center h-full">
+        <span className="text-slate-400">&copy; crewdeck</span>
+      </div>
+      
+    </div>
+  )
+}
