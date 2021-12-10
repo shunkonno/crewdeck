c68468838fb9521420d905e8ef88d7c7d5fbc92a
skonno <19647466+shun-konno@users.noreply.github.com>
2021-12-10 09:35:59 +0900

    Call API for dev purposes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import useSWR from 'swr'
+// Components
 import { Header } from '../components/Header'
 
 export default function Home() {
+  // 検証のため、index.js に仮置き **********
+  // 実際には AccountContext と コントラクトアドレスを変数化して渡す
+  const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+  // Get token balance for a user's address to validate user's membership in a DAO.
+  // @params {string} user - The public address of the user.
+  // @params {string} contract - The contract address of the NFT for a DAO.
+  // @returns {object} tokenBalance - Returns either the balance or an error.
+  const { data: tokenBalance } = useSWR(
+    `/api/alchemy/ethereum/mainnet/getTokenBalances/?user=${'0x940D64638D419675cED0359cc025e3C90Dbe98B2'}&contract=${'0x633cfA9ac099D18C1B33736892427f0a7c1d120a'}`,
+    fetcher
+  )
+
+  const balanceValue = tokenBalance?.tokenBalance
+  const error = tokenBalance?.error
+
+  console.log({ balanceValue, error })
+  // *************************************
+
   return (
     <>
       <Head>
