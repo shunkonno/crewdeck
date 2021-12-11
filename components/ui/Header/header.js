7e4413138f16e9f3ce932de47c0f7cf3b692c434
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-12-12 05:11:36 +0900

    add header menu

--- components/ui/Header/header.js
+++ components/ui/Header/header.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
+// Assets
+import { ChevronDownIcon, PlusSmIcon } from '@heroicons/react/solid'
+
+// Components
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+
+
+// Contexts
 import { useAccount, useAccountConnect } from '@contexts/AccountContext'
 import { truncateAddress } from '@utils/truncateAddress'
 
@@ -8,10 +19,12 @@ export function Header() {
 
   return (
     <header>
-      <div className="py-sm px-xs">
-        <div className="flex justify-between">
+      <div className="py-xs px-xs">
+        <div className="flex justify-between items-center">
           {/* Logo - START */}
+          <Link href="/"><a>
           <div>Logo</div>
+          </a></Link>
           {/* Logo - END */}
           {/* Connect Wallet - START */}
           <div>
@@ -19,9 +32,46 @@ export function Header() {
             {currentAccount && (
               <div>
                 <div>
-                  <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-slate-100 text-slate-800">
-                    {ensName ? ensName : truncateAddress(currentAccount)}
-                  </span>
+                  {
+                    <div className="text-right">
+                    <Menu as="div" className="relative inline-block text-left">
+                      <div>
+                        <Menu.Button className="inline-flex justify-center w-full pl-4 pr-3 py-2 text-sm font-medium text-white border shadow-md border-slate-300 rounded-lg bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                          <span className="inline-flex items-center text-sm font-medium text-slate-800">
+                            {ensName ? ensName : truncateAddress(currentAccount)}
+                            <ChevronDownIcon className="w-6 h-6 ml-2 text-slate-600" />
+                          </span>
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 w-36 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <div className="px-4 py-2 flex justify-center">
+                            <Menu.Item>
+                              {
+                                <Link href="/jobs/post"><a>
+                                <div className="inline-flex bg-primary text-white text-center px-3 py-2 text-sm font-bold rounded-lg">
+                                    <PlusSmIcon className="w-5 h-5 mr-1" />
+                                    <span>Post Job</span>
+                                </div>
+                                </a></Link>
+                              }
+                            </Menu.Item>
+                            
+                          </div>
+                          
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  </div>
+                  }
                 </div>
               </div>
             )}
