a995a4987a95a1dfa3e5bbdd4364f4307d981cd6
skonno <19647466+shun-konno@users.noreply.github.com>
2021-12-13 14:47:43 +0900

    Minor change

--- pages/index.js
+++ pages/index.js
@@ -134,14 +134,15 @@ export default function Home() {
         {/* Filter - END */}
         {/* Filter Results - START */}
         <main className="flex-1 px-sm max-w-4xl">
-          {jobs.map(job => {
-            return(
-              job.is_public &&
-              <div key={job.id} className="mb-sm">
-                <Link href={`/jobs/${job.id}`}><a>
-                  <div className="w-full border shadow-sm border-slate-300 rounded-md bg-white">
-                    <div className="px-4 py-2">
-                      <div className="result-title">
+          {jobs.map((job) => {
+            return (
+              job.is_public && (
+                <div key={job.id} className="mb-sm">
+                  <Link href={`/jobs/${job.id}`}>
+                    <a>
+                      <div className="w-full border shadow-sm border-slate-300 rounded-md bg-white">
+                        <div className="px-4 py-2">
+                          <div className="result-title">
                             <h2 className="text-lg font-medium truncate">
                               {job.title}
                             </h2>
@@ -175,9 +176,6 @@ export default function Home() {
               )
             )
           })}
-          {/* Result - START */}
-
-          {/* Result - END */}
         </main>
         {/* Filter Results - END */}
       </div>
