6c8db752a46536d3c8f0a161fb802f4c6f2d009c
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-12-14 12:09:46 +0900

    レスポンシブ対応

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
+import { Fragment } from 'react'
 import Link from 'next/link'
 import useSWR from 'swr'
 
 // Assets
-import { SearchIcon } from '@heroicons/react/solid'
+import { SearchIcon, AdjustmentsIcon } from '@heroicons/react/solid'
 
 // Components
 import { BaseLayout } from '@components/ui/Layout'
 import { SEO } from '@components/ui/SEO'
+import { Popover, Transition } from '@headlessui/react'
 
 export default function Home() {
   const jobs = [
@@ -62,9 +64,9 @@ export default function Home() {
     <>
       <SEO title="Crewdeck" description="Crewdeck's Top Page" />
       {/* Grid - START */}
-      <div className="py-md flex spacing-x-4">
+      <div className="py-xs sm:py-md block sm:flex spacing-x-4">
         {/* Filter - START */}
-        <div className="flex-shrink-1 px-sm w-72">
+        <div className="hidden sm:block sm:flex-shrink-1 px-sm w-72">
           <div>
             <label className="block text-sm font-medium text-slate-700">
               Search
@@ -132,12 +134,101 @@ export default function Home() {
           </div>
         </div>
         {/* Filter - END */}
+        {/* Filter SP - START */}
+        <div className="block sm:hidden px-sm">
+          <div className="mt-1 flex gap-2 w-full">
+            <div className="relative rounded-md shadow-sm flex-1">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <SearchIcon className="focus:text-primary text-slate-400 w-6 h-6" />
+              </div>
+              <input
+                className="focus:outline-none focus:ring-primary focus:border-primary block w-full pl-10 py-2 sm:text-sm border border-slate-300 rounded-md"
+                placeholder="Search"
+              />
+            </div>
+            <Popover className="relative flex-shrink-0">
+              <Popover.Button>
+                <div className="bg-white rounded-full shadow-sm  p-2 flex justify-center items-center border border-slate-300">
+                  <AdjustmentsIcon className="h-6 w-6 focus:text-primary text-slate-400"/>
+                </div>
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute z-10 w-screen right-0 max-w-sm -mr-sm mt-3 sm:px-0 lg:max-w-3xl">
+                  <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white">
+                    <div className="p-sm">
+                      <div>
+                        <label className="block text-sm font-medium text-slate-700">
+                          DAO
+                        </label>
+                        <div>
+                          {DAOs.map((dao) => {
+                            return (
+                              <div key={dao.id} className="mt-2 relative flex items-start">
+                                <div className="flex items-center h-5">
+                                  <input
+                                    name="tag"
+                                    type="checkbox"
+                                    className="focus:ring-primary h-4 w-4 text-teal-400  border-gray-300 rounded"
+                                  />
+                                </div>
+                                <div className="ml-3 text-sm truncate">
+                                  <span className="text-gray-500">{dao.name}</span>
+                                </div>
+                              </div>
+                            )
+                          })}
+                        </div>
+                      </div>
+                      <div className="mt-sm">
+                        <label className="block text-sm font-medium text-slate-700">
+                          Tags
+                        </label>
+                        <div className="mt-2">
+                          {tags.map((tag) => {
+                            return (
+                              <div key={tag.id} className="mt-2 relative flex items-start">
+                                <div className="flex items-center h-5">
+                                  <input
+                                    name="tag"
+                                    type="checkbox"
+                                    className="focus:ring-primary h-4 w-4 text-teal-400  border-gray-300 rounded"
+                                  />
+                                </div>
+                                <div className="ml-3 text-sm  truncate">
+                                  <span className="text-gray-500">{tag.name}</span>
+                                </div>
+                              </div>
+                            )
+                          })}
+                        </div>
+                      </div>
+                      <div>
+                        <div className="mt-md bg-primary text-white text-center px-4 py-2 font-semibold rounded-lg shadow-md cursor-pointer">
+                          Search
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+          </div>
+        </div>
+        {/* Filter SP - END */}
         {/* Filter Results - START */}
-        <main className="flex-1 px-sm max-w-4xl">
+        <main className="flex-1 px-sm mt-sm sm:mt-0 max-w-4xl">
           {jobs.map((job) => {
             return (
               job.is_public && (
-                <div key={job.id} className="mb-sm">
+                <div key={job.id} className="mb-4 sm:mb-sm">
                   <Link href={`/jobs/${job.id}`}>
                     <a>
                       <div className="w-full border shadow-sm border-slate-300 rounded-md bg-white">
