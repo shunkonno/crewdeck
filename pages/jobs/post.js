41d74c2ede2f2da4430709b38e0f0a2786bfdf41
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-12-19 11:50:00 +0900

    fix dao image

--- pages/jobs/post.js
+++ pages/jobs/post.js
@@ -274,13 +274,15 @@ export default function PostJob({ daos }) {
                                   </span>
                                 ) : (
                                   <>
-                                    <img
-                                      src={
-                                        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                                      }
-                                      alt=""
-                                      className="flex-shrink-0 h-6 w-6 rounded-full"
-                                    />
+                                    {selectedDao.logo_url &&
+                                      <img
+                                        src={
+                                          selectedDao.logo_url
+                                        }
+                                        alt=""
+                                        className="flex-shrink-0 h-6 w-6 rounded-full"
+                                      />
+                                    }
                                     <span className="ml-3 block truncate text-black">
                                       {selectedDao.name}
                                     </span>
