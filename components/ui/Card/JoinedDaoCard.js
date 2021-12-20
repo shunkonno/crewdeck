874fe4ffee33bf14016bbd2570e238f6b8711052
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-12-20 13:31:06 +0900

    daoOptionsロード中にSkeleton表示

---
+++ components/ui/Card/JoinedDaoCard.js
@@ -0,0 +1,23 @@
+import Skeleton from "react-loading-skeleton"
+import 'react-loading-skeleton/dist/skeleton.css'
+
+export function JoinedDaoCard({dao,isReadyDaoOptions}) {
+  return(
+    <>
+    {isReadyDaoOptions ?
+      <div
+        className="inline-flex shadow-md border border-slate-300 bg-white py-2 px-4 rounded-lg"
+      >      
+        <img
+          src={dao.logo_url}
+          alt=""
+          className="block h-6 w-6 mr-3"
+        />
+        <p>{dao.name}</p>
+      </div>
+    :
+      <Skeleton containerClassName="flex-none w-64 mr-xs h-full" height={"2.5rem"} />
+    }
+    </>
+  )
+}
