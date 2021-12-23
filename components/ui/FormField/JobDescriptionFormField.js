6333a52d99fd50b01a5b9dbdd39614059a1e71f1
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-12-23 18:12:16 +0900

    JobDescriptionFormField作成。ReactHookFormと統合

---
+++ components/ui/FormField/JobDescriptionFormField.js
@@ -0,0 +1,43 @@
+import dynamic from 'next/dynamic'
+
+// Quill Editor - Dynamic import to prevent SSR --- https://github.com/zenoamaro/react-quill
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+import 'react-quill/dist/quill.snow.css'
+
+const quillModules = {
+  toolbar: [
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['blockquote']
+  ]
+}
+
+export function JobDescriptionFormField ({Controller,control}) {
+
+  return(
+    <>
+      <label className="block font-medium text-slate-700">
+        Description
+      </label>
+      <p className="mt-1 text-sm text-slate-500">
+        Describe the job, including some background.
+      </p>
+      <div className="mt-1">
+      <Controller
+        control={control}
+        name="editorContent"
+        render={({ 
+          field: { onChange, value } 
+        }) => (
+          <ReactQuill
+            theme="snow"
+            modules={quillModules}
+            value={value || ""}
+            onChange={(editorContent) => onChange(editorContent)}
+          />
+        )}
+      />
+      </div>
+    </>
+  )
+}
