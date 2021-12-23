5c0f6c141d86f95523f4eaa31f05f820d8e291d4
ttokdev <48268149+ttokdev@users.noreply.github.com>
2021-12-23 18:10:41 +0900

    FormFieldコンポーネント作成

---
+++ components/ui/FormField/index.js
@@ -0,0 +1,13 @@
+import { JobTitleFormField } from './JobTitleFormField'
+import { JobDescriptionFormField } from './JobDescriptionFormField'
+import { JobDaoFormField } from './JobDaoFormField'
+import { JobTagsFormField } from './JobTagsFormField'
+import { JobPublicSettingsFormField } from './JobPublicSettingsFormField'
+
+export { 
+  JobTitleFormField, 
+  JobDescriptionFormField, 
+  JobDaoFormField, 
+  JobTagsFormField, 
+  JobPublicSettingsFormField 
+}
