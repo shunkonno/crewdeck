5564c0dbb9b117a9c9d391c3c9d98eeae2176ada
skonno <19647466+shun-konno@users.noreply.github.com>
2022-01-05 20:11:53 +0900

    Remove context parent component

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,14 @@
-import { AccountProvider } from '@contexts/AccountContext'
 import '@styles/globals.css'
 
-const NoSetLayout = ({children}) => <>{children}</>
+const NoSetLayout = ({ children }) => <>{children}</>
 
 function MyApp({ Component, pageProps }) {
-
   const Layout = Component.Layout ?? NoSetLayout
 
   return (
-    <AccountProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AccountProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
