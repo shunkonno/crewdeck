a63476cc4c14bd6073970bc67f4e6094b94ca701
ttokdev <48268149+ttokdev@users.noreply.github.com>
2022-01-06 16:25:23 +0900

    dashboardでselectedDaoを選択可能に

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,21 +1,47 @@
+import { useState, useEffect } from 'react'
 // Supabase
 import { supabase } from '@libs/supabase'
 
 // Components
 import { BaseLayout } from '@components/ui/Layout'
 import { MetaTags } from '@components/ui/MetaTags'
+import { DaoSelectBox } from '@components/ui/SelectBox'
+
+// Functions
+import { detectJoinedDaos } from '@utils/detectJoinedDaos'
+import { AccountProvider } from '@libs/accountProvider'
 
 export default function Dashboard({ daos }) {
   console.log({ daos })
+  const accountProvider = AccountProvider()
+  const { currentAccount, ethersProvider } = accountProvider
+
+
+  // Dao Options UI Control State
+  const [selectedDao, setSelectedDao] = useState(null)
+  const [daoSelectorOptions, setDaoSelectorOptions] = useState([])
+  const [daoSelectorIsReady, setDaoSelectorIsReady] = useState(false)
+
+  console.log(selectedDao)
+
+  useEffect(() => {
+    if (currentAccount) {
+      detectJoinedDaos(daos, currentAccount, setDaoSelectorOptions).then(() => {
+        setDaoSelectorIsReady(true)
+      })
+    }
+  }, [currentAccount, daos])
 
   const people = [
     {
+      id: '1',
       name: 'Jane Cooper',
       title: 'Regional Paradigm Technician',
       role: 'Admin',
       email: 'jane.cooper@example.com'
     },
     {
+      id: '2',
       name: 'Jane Cooper',
       title: 'Regional Paradigm Technician',
       role: 'Admin',
@@ -30,75 +56,85 @@ export default function Dashboard({ daos }) {
   return (
     <>
       <MetaTags title="Dashboard" description="Dashboard" />
-      {/* Table --- START */}
-      <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Title
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Email
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Role
-                    </th>
-                    <th scope="col" className="relative px-6 py-3">
-                      <span className="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {people.map((person) => (
-                    <tr key={person.email}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {person.name}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {person.title}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {person.email}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {person.role}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit
-                        </a>
-                      </td>
+      <div className='max-w-7xl mx-auto'>
+        <div className="mt-sm w-2/3 sm:w-1/3">
+          <DaoSelectBox 
+            selectedDao={selectedDao}
+            setSelectedDao={setSelectedDao}
+            daoSelectorIsReady={daoSelectorIsReady}
+            daoSelectorOptions={daoSelectorOptions}
+          />
+        </div>
+        {/* Table --- START */}
+        <div className="flex flex-col mt-sm">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-slate-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-slate-200">
+                  <thead className="bg-slate-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider"
+                      >
+                        Title
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider"
+                      >
+                        Email
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider"
+                      >
+                        Role
+                      </th>
+                      <th scope="col" className="relative px-6 py-3">
+                        <span className="sr-only">Edit</span>
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-slate-200">
+                    {people.map((person) => (
+                      <tr key={person.id}>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-slate-900">
+                          {person.name}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-slate-500">
+                          {person.title}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-slate-500">
+                          {person.email}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-slate-500">
+                          {person.role}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                          <a
+                            href="#"
+                            className="text-indigo-600 hover:text-indigo-900"
+                          >
+                            Edit
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
+        {/* Table --- END */}
       </div>
-      {/* Table --- END */}
     </>
   )
 }
