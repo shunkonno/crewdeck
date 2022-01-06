bd0a3a296870a30098cfc8fac5ef96dd493e29df
ttokdev <48268149+ttokdev@users.noreply.github.com>
2022-01-06 14:42:10 +0900

    JobTitleのTextInputコンポーネントを挿入

--- components/ui/FormField/JobTitleFormField.js
+++ components/ui/FormField/JobTitleFormField.js
@@ -1,37 +1,22 @@
-import { useState } from 'react'
-import { useForm, Controller } from 'react-hook-form'
+import { JobTitleTextInput } from '@components/ui/TextInput'
 
 export function JobTitleFormField({ errors, register }) {
-  const [title, setTitle] = useState('')
-
   return (
     <>
-      <div>
-        <legend className="sr-only">Title</legend>
-        <label className="block font-medium text-slate-700">Title</label>
-        <p className="mt-1 text-sm text-slate-500">Try to make it concise.</p>
-      </div>
-      {/* Input - START */}
-      <div>
-        <input
-          type="text"
-          className="mt-1 py-2 shadow-sm border focus:outline-none focus:border-primary px-2 block w-full rounded-md border-slate-300"
-          onChange={(e) => {
-            setTitle(e.target.value)
-          }}
-          {...register('title', {
-            required: true,
-            maxLength: 200
-          })}
-        />
-      </div>
-      {/* Input - END */}
+      <label className="block font-medium text-slate-700">Title</label>
+      <p className="mt-1 text-sm text-slate-500">
+        This is the first thing shown. Try to make it concise.
+      </p>
+      <JobTitleTextInput register={register} />
       <div className="mt-2">
         {errors?.title?.type === 'required' && (
           <p className="text-red-400 text-sm">This input is required.</p>
         )}
+        {errors?.title?.type === 'minLength' && (
+          <p className="text-red-400 text-sm">最小10文字</p>
+        )}
         {errors?.title?.type === 'maxLength' && (
-          <p className="text-red-400 text-sm">Max length is 200 characters.</p>
+          <p className="text-red-400 text-sm">最大80文字</p>
         )}
       </div>
     </>
