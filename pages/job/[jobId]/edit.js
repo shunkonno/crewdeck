b5c0f2af0bbc505ceae06f28369613a79fc9d347
ttokdev <48268149+ttokdev@users.noreply.github.com>
2022-01-13 08:07:22 +0900

    ウォレットアカウントによるedit制御

--- pages/job/[jobId]/edit.js
+++ pages/job/[jobId]/edit.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import sanitizeHTML from 'sanitize-html'
@@ -6,18 +7,27 @@ import { useRouter } from 'next/router'
 // Supabase
 import { supabase } from '@libs/supabase'
 
+// Assets
+import { ChevronDownIcon } from '@heroicons/react/outline'
+
 // Functions
+import { detectJoinedDaos } from '@utils/detectJoinedDaos'
+import { verifyAddressOwnership } from '@utils/verifyAddressOwnership'
 import { AccountProvider } from '@libs/accountProvider'
 
 // Components
 import { BaseLayout } from '@components/ui/Layout'
 import { MetaTags } from '@components/ui/MetaTags'
+import { JobTagsCheckboxes } from '@components/ui/Checkboxes'
+
+import { useForm, Controller } from 'react-hook-form'
 
 // Constants
 import { statusOptions } from '@constants/statusOptions'
+import { DaoSelectBox } from '@components/ui/SelectBox'
 
-export default function Job({ job, dao }) {
-  console.log({ job, dao })
+export default function Job({ job, dao, tags, daos }) {
+  console.log({ job, dao, tags })
 
   // ****************************************
   // ACCOUNT
@@ -31,6 +41,56 @@ export default function Job({ job, dao }) {
   // ****************************************
 
   const router = useRouter()
+
+  // ****************************************
+  // UI CONTROL STATE
+  // ****************************************
+
+  // Dao Options UI Control State
+  const [daoSelectorOptions, setDaoSelectorOptions] = useState([])
+  const [daoSelectorIsReady, setDaoSelectorIsReady] = useState(false)
+  const [isEditAuth, setIsEditAuth] = useState(false)
+
+
+  useEffect(() => {
+    if (currentAccount) {
+      detectJoinedDaos(daos, currentAccount, setDaoSelectorOptions).then(() => {
+        setDaoSelectorIsReady(true)
+      })
+    }
+  }, [currentAccount, daos])
+
+  useEffect(() => {
+    const editable = daoSelectorOptions.some((detectedDao) => {
+      return detectedDao.dao_id == job.dao_id
+    })
+
+    setIsEditAuth(editable)
+  }, [daoSelectorOptions, job.dao_id])
+
+  // ****************************************
+  // FORM SETTINGS
+  // ****************************************
+  const [title, setTitle] = useState(job.title || '')
+  const [description, setDescription] = useState(null)
+  const [selectedDao, setSelectedDao] = useState(dao)
+
+console.log(selectedDao)
+  // tagsの数だけtag_idをキーの持つ、value falseのオブジェクトを作る
+  const tagsStateObject = tags.reduce((accum, tag) => {
+    return { ...accum, [tag.tag_id]: false }
+  }, {})
+  const [selectedTags, setSelectedTags] = useState(tagsStateObject)
+
+  // React Hook Form Setting
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm()
+
+  Object.keys(errors).length && console.log('react-hook-form Errors', errors)
 
   // ****************************************
   // HANDLE INNER HTML
@@ -77,25 +137,10 @@ export default function Job({ job, dao }) {
   }
 
   // ****************************************
-  // DB
+  // onSubmit
   // ****************************************
-
-  async function updateJobStatus(e) {
-    e.preventDefault()
-
-    console.log(currentAccount)
-
-    const { data, error } = await supabase
-      .from('jobs')
-      .update({ status: statusOptions[1], lead_contributor: currentAccount })
-      .match({ job_id: job.job_id })
-
-    if (error) {
-      console.log(error)
-      return
-    }
-
-    router.reload()
+  function onTestSubmit(data) {
+    console.log(data)
   }
 
   // ****************************************
@@ -108,107 +153,120 @@ export default function Job({ job, dao }) {
         title="Crewdeck - Job Detail"
         description="Manage job and bounties for your DAO with Crewdeck. Check jobs to work for a DAO. "
       />
-      <div className="max-w-7xl mx-auto py-md ">
-        <div className="mb-2 flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2 max-w-7xl mx-auto">
-          <div className="lg:flex-1 flex justify-end">
-            <div>
-              <Link href={`/job/${job.job_id}`}>
-                <a className="my-1 text-slate-600 mr-6 text-lg inline-block rounded-lg hover:text-slate-800">
-                  Cancel
-                </a>
-              </Link>
-              <div className="px-xs py-1 bg-primary text-white text-lg inline-block rounded-lg cursor-pointer hover:bg-primary-hover hover:text-slate-100">
-                Save
+      {/* Wrapper Component -- START */}
+      <div className="max-w-7xl mx-auto py-md">
+      {!isEditAuth ? 
+        <p>You don't have the authentication to edit this job information</p>
+      :
+        <form onSubmit={handleSubmit(onTestSubmit)}>
+          <div className="mb-2 flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2 max-w-7xl mx-auto">
+            <div className="lg:flex-1 flex justify-end">
+              <div>
+                <Link href={`/job/${job.job_id}`}>
+                  <a className="my-1 text-slate-600 mr-6 text-lg inline-block rounded-lg hover:text-slate-800">
+                    Cancel
+                  </a>
+                </Link>
+                <button 
+                  className="px-xs py-1 bg-primary text-white text-lg inline-block rounded-lg cursor-pointer hover:bg-primary-hover hover:text-slate-100"
+                  type="submit"
+                >
+                  Save
+                </button>
               </div>
             </div>
+            <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80"></div>
           </div>
-          <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80"></div>
-        </div>
-        {/* Grid - START */}
-        <div className="flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2">
-          {/* Job - START */}
-          <div className="lg:flex-1 bg-white p-xs border border-slate-300 rounded-lg">
-            {/* Job Title - START */}
-            <div>
-              <h1 className="text-2xl font-semibold">{job.title}</h1>
-            </div>
-            {/* Job Title - END */}
-            {/* Job Tags - START */}
-            <div className="mt-4">
-              <div className="flex flex-wrap justify-start gap-2">
-                {job.tags.map((tag) => (
-                  <div key={tag.id}>
-                    <span className="inline-block items-center px-2 py-0.5 rounded text-sm font-medium bg-slate-200 text-slate-800">
-                      {tag.name}
-                    </span>
-                  </div>
-                ))}
+          
+          {/* Grid - START */}
+          <div className="flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2">
+            {/* Job - START */}
+            <div className="lg:flex-1 bg-white p-xs border border-slate-300 rounded-lg">
+              {/* Job Title - START */}
+              <div>
+                <input 
+                  type="text" 
+                  className="text-2xl font-medium border-b border-slate-400 w-full outline-none focus:border-teal-400" 
+                  onChange={((e) => setTitle(e.target.value))}
+                  value={title}
+                  {...register('title', {
+                    required: true,
+                    maxLength: 200
+                  })}
+                />
               </div>
+              {/* Job Title - END */}
+              {/* Job Tags - START */}
+              <div className="mt-4">
+                <JobTagsCheckboxes 
+                  tags={tags} 
+                  register={register}
+                  selectedTags={selectedTags} 
+                  setSelectedTags={setSelectedTags}
+                />
+              </div>
+              {/* Job Tags - END */}
+              {/* Job Description - START */}
+              <div className="mt-lg">
+                <article
+                  className="prose"
+                  dangerouslySetInnerHTML={composeInnerHTML(job.description)}
+                />
+              </div>
+              {/* Job Description - END */}
             </div>
-            {/* Job Tags - END */}
-            {/* Job Description - START */}
-            <div className="mt-lg">
-              <article
-                className="prose"
-                dangerouslySetInnerHTML={composeInnerHTML(job.description)}
-              />
-            </div>
-            {/* Job Description - END */}
-          </div>
-          {/* Job - END */}
-          {/* Sidebar - START */}
-          <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80">
-            <div className="bg-white border border-slate-300 p-xs rounded-lg">
-              <div className="inline-flex items-center">
-                {dao.logo_url && (
-                  <div className="relative w-8 h-8 flex-shrink-0 rounded-full mr-3">
-                    <Image
-                      src={dao.logo_url}
-                      layout={'fill'}
-                      className="absolute inline-block h-8 w-8 "
-                    />
+            {/* Job - END */}
+            {/* Sidebar - START */}
+            <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80">
+              <div className="bg-white border border-slate-300 p-xs rounded-lg">
+                <DaoSelectBox 
+                  selectedDao={selectedDao}
+                  setSelectedDao={setSelectedDao}
+                  daoSelectorIsReady={daoSelectorIsReady}
+                  daoSelectorOptions={daoSelectorOptions}
+                  label={'Select DAO'}
+                />
+                <p className="mt-2 text-slate-600">{selectedDao.description}</p>
+                {(selectedDao.discord_url || selectedDao.twitter_url) && (
+                  <div className="inline-flex gap-4 mt-xs">
+                    {selectedDao.discord_url && (
+                      <a
+                        className="relative inline-block w-6 h-6 flex-shrink-0"
+                        href={selectedDao.discord_url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Image
+                          src={'/images/social/DiscordIcon.png'}
+                          layout={'fill'}
+                          className=""
+                        />
+                      </a>
+                    )}
+                    {selectedDao.twitter_url && (
+                      <a
+                        className="relative inline-block w-6 h-6 flex-shrink-0"
+                        href={selectedDao.twitter_url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <Image
+                          layout={'fill'}
+                          src={'/images/social/TwitterIcon.png'}
+                        />
+                      </a>
+                    )}
                   </div>
                 )}
-                <h1 className="text-lg font-semibold">{dao.name}</h1>
               </div>
-              <p className="mt-1 text-slate-600">{dao.description}</p>
-              {(dao.discord_url || dao.twitter_url) && (
-                <div className="inline-flex gap-4 mt-xs">
-                  {dao.discord_url && (
-                    <a
-                      className="relative inline-block w-6 h-6 flex-shrink-0"
-                      href={dao.discord_url}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <Image
-                        src={'/images/social/DiscordIcon.png'}
-                        layout={'fill'}
-                        className=""
-                      />
-                    </a>
-                  )}
-                  {dao.twitter_url && (
-                    <a
-                      className="relative inline-block w-6 h-6 flex-shrink-0"
-                      href={dao.twitter_url}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <Image
-                        layout={'fill'}
-                        src={'/images/social/TwitterIcon.png'}
-                      />
-                    </a>
-                  )}
-                </div>
-              )}
             </div>
+            {/* Sidebar - END */}
           </div>
-          {/* Sidebar - END */}
-        </div>
-        {/* Grid - END */}
+          {/* Grid - END */}
+        </form>
+      }
       </div>
+      {/* Wrapper Component -- END */}
     </>
   )
 }
@@ -278,5 +336,13 @@ export const getStaticProps = async ({ params: { jobId } }) => {
     .eq('dao_id', job.dao_id)
     .single()
 
-  return { props: { job, dao } }
+  const { data: daos } = await supabase
+  .from('daos')
+  .select('*')
+
+  const { data: tags } = await supabase
+    .from('tags')
+    .select('*')
+
+  return { props: { job, dao, tags, daos } }
 }
