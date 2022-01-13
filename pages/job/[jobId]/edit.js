e4881b180808d6ba00acf2aa3d732aad6582a869
ttokdev <48268149+ttokdev@users.noreply.github.com>
2022-01-14 06:27:21 +0900

    editForm作成

--- pages/job/[jobId]/edit.js
+++ pages/job/[jobId]/edit.js
@@ -1,14 +1,11 @@
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import sanitizeHTML from 'sanitize-html'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 
 // Supabase
 import { supabase } from '@libs/supabase'
-
-// Assets
-import { ChevronDownIcon } from '@heroicons/react/outline'
 
 // Functions
 import { detectJoinedDaos } from '@utils/detectJoinedDaos'
@@ -19,6 +16,7 @@ import { AccountProvider } from '@libs/accountProvider'
 import { BaseLayout } from '@components/ui/Layout'
 import { MetaTags } from '@components/ui/MetaTags'
 import { JobTagsCheckboxes } from '@components/ui/Checkboxes'
+import { JobTitleTextArea } from '@components/ui/TextInput'
 
 import { useForm, Controller } from 'react-hook-form'
 
@@ -26,8 +24,12 @@ import { useForm, Controller } from 'react-hook-form'
 import { statusOptions } from '@constants/statusOptions'
 import { DaoSelectBox } from '@components/ui/SelectBox'
 
+// Quill Editor - Dynamic import to prevent SSR --- https://github.com/zenoamaro/react-quill
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+import 'react-quill/dist/quill.snow.css'
+
 export default function Job({ job, dao, tags, daos }) {
-  console.log({ job, dao, tags })
+  // console.log({ job, dao, tags })
 
   // ****************************************
   // ACCOUNT
@@ -51,7 +53,6 @@ export default function Job({ job, dao, tags, daos }) {
   const [daoSelectorIsReady, setDaoSelectorIsReady] = useState(false)
   const [isEditAuth, setIsEditAuth] = useState(false)
 
-
   useEffect(() => {
     if (currentAccount) {
       detectJoinedDaos(daos, currentAccount, setDaoSelectorOptions).then(() => {
@@ -71,16 +72,17 @@ export default function Job({ job, dao, tags, daos }) {
   // ****************************************
   // FORM SETTINGS
   // ****************************************
-  const [title, setTitle] = useState(job.title || '')
-  const [description, setDescription] = useState(null)
-  const [selectedDao, setSelectedDao] = useState(dao)
-
-console.log(selectedDao)
   // tagsの数だけtag_idをキーの持つ、value falseのオブジェクトを作る
   const tagsStateObject = tags.reduce((accum, tag) => {
     return { ...accum, [tag.tag_id]: false }
   }, {})
+
+  const [title, setTitle] = useState(job.title || '')
+  const [editorContent, setEditorContent] = useState(job.description)
+  const [selectedDao, setSelectedDao] = useState(dao)
   const [selectedTags, setSelectedTags] = useState(tagsStateObject)
+
+  console.log('Title: ', title)
 
   // React Hook Form Setting
   const {
@@ -93,54 +95,28 @@ console.log(selectedDao)
   Object.keys(errors).length && console.log('react-hook-form Errors', errors)
 
   // ****************************************
-  // HANDLE INNER HTML
+  // QUILL SETTINGS
   // ****************************************
-
-  // Sanitizes HTML.
-  // @param {string} rawHtml
-  // @returns {string} sanitizedHtml
-  function composeSanitizedHTML(rawHTML) {
-    const options = {
-      allowedTags: [
-        'h1',
-        'h2',
-        'h3',
-        'li',
-        'ol',
-        'p',
-        'ul',
-        'a',
-        'br',
-        'u',
-        'em',
-        'strong',
-        'blockquote'
-      ],
-      allowedAttributes: {
-        a: ['href', 'rel', 'target']
-      }
-    }
-
-    const sanitizedHTML = sanitizeHTML(rawHTML, options)
-
-    return sanitizedHTML
-  }
-
-  // Returns object to set using dangerouslySetInnerHTML
-  // @param {string} rawHtml
-  // @returns {striing} sanitizedHtml
-  function composeInnerHTML(rawHTML) {
-    // Sanitize HTML.
-    const sanitizedHTML = composeSanitizedHTML(rawHTML)
-    // Return HTML as object.
-    return { __html: sanitizedHTML }
+  const quillModules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['blockquote']
+    ]
   }
 
   // ****************************************
   // onSubmit
   // ****************************************
-  function onTestSubmit(data) {
-    console.log(data)
+  function onSubmit(validatedData) {
+    const submitData = {
+      ...validatedData,
+      editorContent,
+      selectedDao
+    }
+
+    console.log('submitData:', submitData)
+
   }
 
   // ****************************************
@@ -150,120 +126,160 @@ console.log(selectedDao)
   return (
     <>
       <MetaTags
-        title="Crewdeck - Job Detail"
+        title="Crewdeck - Edit Job"
         description="Manage job and bounties for your DAO with Crewdeck. Check jobs to work for a DAO. "
       />
       {/* Wrapper Component -- START */}
       <div className="max-w-7xl mx-auto py-md">
-      {!isEditAuth ? 
-        <p>You don't have the authentication to edit this job information</p>
+      {!daoSelectorIsReady ?
+        /*  Loading Spinner -- START */
+        <div className='inline-flex justify-center w-full'>
+          <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-slate-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </div>
+        /*  Loading Spinner -- END */
       :
-        <form onSubmit={handleSubmit(onTestSubmit)}>
-          <div className="mb-2 flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2 max-w-7xl mx-auto">
-            <div className="lg:flex-1 flex justify-end">
-              <div>
-                <Link href={`/job/${job.job_id}`}>
-                  <a className="my-1 text-slate-600 mr-6 text-lg inline-block rounded-lg hover:text-slate-800">
-                    Cancel
-                  </a>
-                </Link>
-                <button 
-                  className="px-xs py-1 bg-primary text-white text-lg inline-block rounded-lg cursor-pointer hover:bg-primary-hover hover:text-slate-100"
-                  type="submit"
-                >
-                  Save
-                </button>
+        !isEditAuth ? 
+          <p>You don't have the authentication to edit this job information</p>
+        :
+          /*  Form -- START */
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-2 flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2 max-w-7xl mx-auto">
+              <div className="lg:flex-1 flex justify-end">
+                <div>
+                  <Link href={`/job/${job.job_id}`}>
+                    <a className="my-1 text-slate-600 mr-6 text-lg inline-block rounded-lg hover:text-slate-800">
+                      Cancel
+                    </a>
+                  </Link>
+                  <button 
+                    className="px-xs py-1 bg-primary text-white text-lg inline-block rounded-lg cursor-pointer hover:bg-primary-hover hover:text-slate-100"
+                    type="submit"
+                  >
+                    Save
+                  </button>
+                </div>
               </div>
+              <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80"></div>
             </div>
-            <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80"></div>
-          </div>
+            
+            {/* Grid - START */}
+            <div className="flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2">
+              {/* Job - START */}
+              <div className="lg:flex-1 bg-white p-xs border border-slate-300 rounded-lg">
+                {/* Job Title - START */}
+                <div>
+                  <fieldset>
+                    <legend className='sr-only'>Title</legend>
+                    <Controller
+                      control={control}
+                      name="title"
+                      defaultValue={job.title}
+                      rules={{ required: true, maxLength: 200 }}
+                      render={({ field: { onChange, value, name } }) => (
+                        <JobTitleTextArea
+                          onChange={onChange}
+                          title={value}
+                          control={control}
+                          name={name}
+                          setTitle={setTitle}
+                        />
+                      )}
+                    />
+                    {/* <textarea 
+                      className="text-lg sm:text-2xl font-medium border-b border-slate-200 w-full outline-none focus:border-teal-400" 
+                      onChange={((e) => setTextareaTitle(e.target.value))}
+                      onInput={()=> setTitle(textareaTitle.replace(/\n/g, ''))}
+                      value={textareaTitle}
+                    />
+                    <input 
+                      type='text'
+                      className='hidden' 
+                      name='title'
+                      value={title} 
+                      {...register('title', {
+                        required: true,
+                        maxLength: 200
+                      })}
+                    /> */}
+                  </fieldset>
+                </div>
+                {/* Job Title - END */}
+                {/* Job Tags - START */}
+                <div className="mt-4">
+                  <JobTagsCheckboxes 
+                    tags={tags} 
+                    register={register}
+                    selectedTags={selectedTags} 
+                    setSelectedTags={setSelectedTags}
+                  />
+                </div>
+                {/* Job Tags - END */}
+
+                {/* Job Description - START */}
+                <div className="mt-lg">
+                  <ReactQuill
+                    theme="snow"
+                    modules={quillModules}
+                    value={editorContent}
+                    onChange={setEditorContent}
+                  />
+                </div>
+                {/* Job Description - END */}
+
+              </div>
+              {/* Job - END */}
+              {/* Sidebar - START */}
+              <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80">
+                <div className="bg-white border border-slate-300 p-xs rounded-lg">
+                  <DaoSelectBox 
+                    selectedDao={selectedDao}
+                    setSelectedDao={setSelectedDao}
+                    daoSelectorIsReady={daoSelectorIsReady}
+                    daoSelectorOptions={daoSelectorOptions}
+                    label={'Select DAO'}
+                  />
+                  <p className="mt-2 text-slate-600">{selectedDao.description}</p>
+                  {(selectedDao.discord_url || selectedDao.twitter_url) && (
+                    <div className="inline-flex gap-4 mt-xs">
+                      {selectedDao.discord_url && (
+                        <a
+                          className="relative inline-block w-6 h-6 flex-shrink-0"
+                          href={selectedDao.discord_url}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <Image
+                            src={'/images/social/DiscordIcon.png'}
+                            layout={'fill'}
+                            className=""
+                          />
+                        </a>
+                      )}
+                      {selectedDao.twitter_url && (
+                        <a
+                          className="relative inline-block w-6 h-6 flex-shrink-0"
+                          href={selectedDao.twitter_url}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <Image
+                            layout={'fill'}
+                            src={'/images/social/TwitterIcon.png'}
+                          />
+                        </a>
+                      )}
+                    </div>
+                  )}
+                </div>
+              </div>
+              {/* Sidebar - END */}
+            </div>
+            {/* Grid - END */}
+          </form>
           
-          {/* Grid - START */}
-          <div className="flex flex-col lg:flex-row px-4 lg:px-xs lg:gap-2">
-            {/* Job - START */}
-            <div className="lg:flex-1 bg-white p-xs border border-slate-300 rounded-lg">
-              {/* Job Title - START */}
-              <div>
-                <input 
-                  type="text" 
-                  className="text-2xl font-medium border-b border-slate-400 w-full outline-none focus:border-teal-400" 
-                  onChange={((e) => setTitle(e.target.value))}
-                  value={title}
-                  {...register('title', {
-                    required: true,
-                    maxLength: 200
-                  })}
-                />
-              </div>
-              {/* Job Title - END */}
-              {/* Job Tags - START */}
-              <div className="mt-4">
-                <JobTagsCheckboxes 
-                  tags={tags} 
-                  register={register}
-                  selectedTags={selectedTags} 
-                  setSelectedTags={setSelectedTags}
-                />
-              </div>
-              {/* Job Tags - END */}
-              {/* Job Description - START */}
-              <div className="mt-lg">
-                <article
-                  className="prose"
-                  dangerouslySetInnerHTML={composeInnerHTML(job.description)}
-                />
-              </div>
-              {/* Job Description - END */}
-            </div>
-            {/* Job - END */}
-            {/* Sidebar - START */}
-            <div className="lg:flex-shrink-1 mt-md sm:mt-0 w-full lg:w-80">
-              <div className="bg-white border border-slate-300 p-xs rounded-lg">
-                <DaoSelectBox 
-                  selectedDao={selectedDao}
-                  setSelectedDao={setSelectedDao}
-                  daoSelectorIsReady={daoSelectorIsReady}
-                  daoSelectorOptions={daoSelectorOptions}
-                  label={'Select DAO'}
-                />
-                <p className="mt-2 text-slate-600">{selectedDao.description}</p>
-                {(selectedDao.discord_url || selectedDao.twitter_url) && (
-                  <div className="inline-flex gap-4 mt-xs">
-                    {selectedDao.discord_url && (
-                      <a
-                        className="relative inline-block w-6 h-6 flex-shrink-0"
-                        href={selectedDao.discord_url}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <Image
-                          src={'/images/social/DiscordIcon.png'}
-                          layout={'fill'}
-                          className=""
-                        />
-                      </a>
-                    )}
-                    {selectedDao.twitter_url && (
-                      <a
-                        className="relative inline-block w-6 h-6 flex-shrink-0"
-                        href={selectedDao.twitter_url}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <Image
-                          layout={'fill'}
-                          src={'/images/social/TwitterIcon.png'}
-                        />
-                      </a>
-                    )}
-                  </div>
-                )}
-              </div>
-            </div>
-            {/* Sidebar - END */}
-          </div>
-          {/* Grid - END */}
-        </form>
       }
       </div>
       {/* Wrapper Component -- END */}
